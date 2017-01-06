782fb83de8604c7350c9997e8d59b5c0d9e2c42d
dygks910910 <dygks910910@daum.net>
2017-01-07 06:25:08 +0900

    Base를 파티클시스템 으로 변경.

--- Client/Vertex.h
+++ Client/Vertex.h
@@ -1,86 +1,61 @@
-#pragma once
-#include "MathHelperFBX.h"
-#include <vector>
-#include <algorithm>
+//***************************************************************************************
+// Vertex.h by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Defines vertex structures and input layouts.
+//***************************************************************************************
 
+#ifndef VERTEX_H
+#define VERTEX_H
 
-struct Vertex
+#include "d3dUtil.h"
+
+namespace Vertex
 {
-	XMFLOAT3 Pos;
-	XMFLOAT4 Color;
+	// Basic 32-byte vertex structure.
+	struct Basic32
+	{
+		XMFLOAT3 Pos;
+		XMFLOAT3 Normal;
+		XMFLOAT2 Tex;
+	};
+
+	struct Terrain
+	{
+		XMFLOAT3 Pos;
+		XMFLOAT2 Tex;
+		XMFLOAT2 BoundsY;
+	};
+
+	struct Particle
+	{
+		XMFLOAT3 InitialPos;
+		XMFLOAT3 InitialVel;
+		XMFLOAT2 Size;
+		float Age;
+		unsigned int Type;
+	};
+}
+
+class InputLayoutDesc
+{
+public:
+	// Init like const int A::a[4] = {0, 1, 2, 3}; in .cpp file.
+	static const D3D11_INPUT_ELEMENT_DESC Pos[1];
+	static const D3D11_INPUT_ELEMENT_DESC Basic32[3];
+	static const D3D11_INPUT_ELEMENT_DESC Terrain[3];
+	static const D3D11_INPUT_ELEMENT_DESC Particle[5];
 };
 
-struct PNTVertex
+class InputLayouts
 {
-	XMFLOAT3 mPosition;
-	XMFLOAT3 mNormal;
-	XMFLOAT2 mUV;
+public:
+	static void InitAll(ID3D11Device* device);
+	static void DestroyAll();
 
-	bool operator==(const PNTVertex& rhs) const
-	{
-		uint32_t position;
-		uint32_t normal;
-		uint32_t uv;
-
-		XMVectorEqualR(&position, XMLoadFloat3(&(this->mPosition)), XMLoadFloat3(&rhs.mPosition));
-		XMVectorEqualR(&normal, XMLoadFloat3(&(this->mNormal)), XMLoadFloat3(&rhs.mNormal));
-		XMVectorEqualR(&uv, XMLoadFloat2(&(this->mUV)), XMLoadFloat2(&rhs.mUV));
-
-		return XMComparisonAllTrue(position) && XMComparisonAllTrue(normal) && XMComparisonAllTrue(uv);
-	}
+	static ID3D11InputLayout* Pos;
+	static ID3D11InputLayout* Basic32;
+	static ID3D11InputLayout* Terrain;
+	static ID3D11InputLayout* Particle;
 };
 
-struct VertexBlendingInfo
-{
-	unsigned int mBlendingIndex;
-	double mBlendingWeight;
-
-	VertexBlendingInfo():
-		mBlendingIndex(0),
-		mBlendingWeight(0.0)
-	{}
-
-	bool operator < (const VertexBlendingInfo& rhs)
-	{
-		return (mBlendingWeight > rhs.mBlendingWeight);
-	}
-};
-
-struct PNTIWVertex
-{
-	XMFLOAT3 mPosition;
-	XMFLOAT3 mNormal;
-	XMFLOAT2 mUV;
-	std::vector<VertexBlendingInfo> mVertexBlendingInfos;
-
-	void SortBlendingInfoByWeight()
-	{
-		std::sort(mVertexBlendingInfos.begin(), mVertexBlendingInfos.end());
-	}
-
-	bool operator==(const PNTIWVertex& rhs) const
-	{
-		bool sameBlendingInfo = true;
-
-		// We only compare the blending info when there is blending info
-		if(!(mVertexBlendingInfos.empty() && rhs.mVertexBlendingInfos.empty()))
-		{
-			// Each vertex should only have 4 index-weight blending info pairs
-			for (unsigned int i = 0; i < 4; ++i)
-			{
-				if (mVertexBlendingInfos[i].mBlendingIndex != rhs.mVertexBlendingInfos[i].mBlendingIndex ||
-					abs(mVertexBlendingInfos[i].mBlendingWeight - rhs.mVertexBlendingInfos[i].mBlendingWeight) > 0.001)
-				{
-					sameBlendingInfo = false;
-					break;
-				}
-			}
-		}
-		
-		bool result1 = MathHelperFBX::CompareVector3WithEpsilon(mPosition, rhs.mPosition);
-		bool result2 = MathHelperFBX::CompareVector3WithEpsilon(mNormal, rhs.mNormal);
-		bool result3 = MathHelperFBX::CompareVector2WithEpsilon(mUV, rhs.mUV);
-
-		return result1 && result2 && result3 && sameBlendingInfo;
-	}
-};
+#endif // VERTEX_H
